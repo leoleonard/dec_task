3579ac2d4a56054a16eec80f11fc24f3038c0b25
Damian Juzyk <damian.juzyk@amrest.eu>
2022-02-10 11:40:43 +0100

    add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule
